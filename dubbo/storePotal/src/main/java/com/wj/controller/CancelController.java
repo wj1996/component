ab3459f15a8f881132713aba779a629ec20fd9c3
jie8023yu <2425393776@qq.com>
2019-08-20 23:41:43 +0800

    回声测试

--- dubbo/storePotal/src/main/java/com/wj/controller/CancelController.java
+++ dubbo/storePotal/src/main/java/com/wj/controller/CancelController.java
@@ -2,14 +2,20 @@ package com.wj.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.alibaba.dubbo.rpc.RpcContext;
+import com.alibaba.dubbo.rpc.service.EchoService;
 import com.wj.service.OrderService;
 import com.wj.service.PayService;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
@@ -17,7 +23,9 @@ import java.util.concurrent.Future;
  * 演示异步调用（dubbo官方不建议使用）
  */
 @Controller
-public class CancelController {
+public class CancelController implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
 
     @Autowired
     private PayService payService;
@@ -57,5 +65,30 @@ public class CancelController {
         orderService.submit(money);
         request.setAttribute("order",detail);
         return "order";
+    }
+
+    /**
+     * 回声测试
+     */
+    @RequestMapping("test2")
+    @ResponseBody
+    public HashMap test(HttpServletRequest request) {
+        String[] serviceIds = new String[]{"orderService","userService","vipUserService","payService"};
+        HashMap<String,String> map = new HashMap<String, String>();
+        Object ret = null;
+        for (String id : serviceIds) {
+            try {
+                EchoService echoService = (EchoService) applicationContext.getBean(id);
+                map.put(id,echoService.$echo("ok").toString());
+            } catch (Exception e) {
+                map.put(id,"not ready");
+            }
+        }
+
+        return map;
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
