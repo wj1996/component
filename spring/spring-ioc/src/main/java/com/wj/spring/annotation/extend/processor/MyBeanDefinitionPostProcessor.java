0641204d73cb594a9d15a2cbe2803bc0b3256d62
jie8023yu <2425393776@qq.com>
2019-09-19 23:46:08 +0800

    s

--- spring/spring-ioc/src/main/java/com/wj/spring/annotation/extend/processor/MyBeanDefinitionPostProcessor.java
+++ spring/spring-ioc/src/main/java/com/wj/spring/annotation/extend/processor/MyBeanDefinitionPostProcessor.java
@@ -1,11 +1,10 @@
 package com.wj.spring.annotation.extend.processor;
 
 import com.wj.spring.annotation.cls.Dog;
+import com.wj.spring.annotation.cls2.Moon;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
-import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.beans.factory.support.*;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,8 +13,14 @@ public class MyBeanDefinitionPostProcessor implements BeanDefinitionRegistryPost
 
     public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
         System.out.println("MyBeanDefinitionPostProcessor....postProcessBeanDefinitionRegistry,bean的数量" + registry.getBeanDefinitionCount());
+
+        //注册bean 方式1
         RootBeanDefinition rootBeanDefinition = new RootBeanDefinition(Dog.class);
         registry.registerBeanDefinition("hello",rootBeanDefinition);
+
+        //注册bean 方式2
+        AbstractBeanDefinition beanDefinition = BeanDefinitionBuilder.rootBeanDefinition(Moon.class).getBeanDefinition();
+        registry.registerBeanDefinition("hello2",beanDefinition);
     }
 
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
