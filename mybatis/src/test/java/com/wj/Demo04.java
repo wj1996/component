fcf47b666799675bda72069398d5035d087cb681
jie8023yu <2425393776@qq.com>
2019-10-08 08:57:48 +0800

    123

---
+++ mybatis/src/test/java/com/wj/Demo04.java
@@ -0,0 +1,28 @@
+package com.wj;
+
+import com.wj.model.TUser;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.InputStream;
+
+/**
+ * 源码分析
+ */
+public class Demo04 {
+
+
+    @Test
+    public void quickStart() throws Exception {
+        InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        //执行查询语句
+        TUser user = sqlSession.selectOne("com.wj.mapper.TUserMapper.selectByPrimaryKey", 1);
+        System.out.println(user);
+
+    }
+}
