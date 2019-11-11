f10ee864698623420a6073208f6eadd9a4d8330d
wj1996 <15236730688@163.com>
2019-11-11 18:54:56 +0800

    nio

--- netty/src/main/java/com/socket/nio/Server022.java
+++ netty/src/main/java/com/socket/nio/Server022.java
@@ -9,8 +9,14 @@ import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class Server02 {
+public class Server022 {
+
+
+    static ExecutorService executorService = Executors.newFixedThreadPool(5);
+
 
 
     public static void main(String[] args) {
@@ -99,81 +105,7 @@ public class Server02 {
                             if (selectionKey.isReadable()) {
                                 System.out.println("服务器开始读取数据");
                                 SocketChannel channel = (SocketChannel) selectionKey.channel();
-
-                                ByteBuffer byteBuffer = ByteBuffer.allocate(1024);   //这里如果分配的大小不足以写入所有客户端的数据时，会等待一次次写入
-                                //将通道中的数据读取到缓存中，通道中的数据就是客户端发送给服务器的数据
-
-                                /**
-                                 * 流程分析
-                                 *  channel.read(byteBuffer);
-                                 *      -> SocketChannelImpl
-                                 *          var3 = IOUtil.read(this.fd, var1, -1L, nd);
-                                 *              -> IOUtil
-                                 *                  int var6 = readIntoNativeBuffer(var0, var5, var2, var4);
-                                 *                      readIntoNativeBuffer
-                                 *                          var9 = var4.read(var0, ((DirectBuffer)var1).address() + (long)var5, var7);   -> var4 ----->SocketDispatcher
-                                 *                              -> SocketDispatcher
-                                 *                                  read0(var1, var2, var4)
-                                 *                                      调用native方法
-                                 *                                      static native int read0(FileDescriptor var0, long var1, int var3) throws IOException;
-                                 *
-                                 *                                      翻译openjdk这里的源代码
-                                 *                                          Java_sun_nio_ch_SocketDispatcher_read0(JNIEnv *env, jclass clazz, jobject fdo,
-                                 *                                       jlong address, jint len)
-                                 *                                          见下，使用Windows的socket套接字编程，首先是将数据读取到定义的buffers中
-                                 *                                          然后，调用函数return convertReturnVal(env, (jint)read, JNI_TRUE); 将数据返回、
-                                 *
-                                 *
-                                 *
-                                 *
-                                 *   经过测试可以发现的状况是：
-                                 *          如果发送数据大小超过了读取时定义的ByteBuffer的大小时，会去读取多次，将数据全部读取到。
-                                 */
-                                /* read into the buffers */
-                                /*i = WSARecv((SOCKET)fd, *//* Socket *//*
-                                        &buf,           *//* pointers to the buffers *//*
-                                        (DWORD)1,       *//* number of buffers to process *//*
-                                        &read,          *//* receives number of bytes read *//*
-                                        &flags,         *//* no flags *//*
-                                        0,              *//* no overlapped sockets *//*
-                                        0);            */ /* no completion routine */
-
-                                int readLength = channel.read(byteBuffer);
-                                if (readLength > 0) {
-                                    //flip NIO中最复杂的操作就是Buffer的控制
-                                    /**
-                                     * Buffer中有一个游标，游标信息在操作后不会归零。如果直接访问Buffer的话，数据有不一致的可能。
-                                     * flip是重置游标的方法。在NIO编程中，flip方法是常用方法
-                                     */
-                                    byteBuffer.flip();
-                                    //字节数组，保存具体数据  byteBuffer.reamining方法是获取Buffer中有效数据长度
-                                    byte[] bytes = new byte[byteBuffer.remaining()];
-                                    //将buffer中的数据保存到bytes字节数组中
-                                    byteBuffer.get(bytes);
-                                    String requestStr = new String(bytes,"utf-8");
-                                    System.out.println("请求的数据来源：" + channel.getRemoteAddress() +",请求的数据为：" + requestStr);
-                                    String respStr = "哈哈";
-                                    if ("1".equals(requestStr)) {
-                                        respStr = "hello world";
-                                    } else if ("2".equals(requestStr)) {
-                                        respStr = "once complie";
-                                    }
-
-                                    System.out.println("服务器开始响应");
-//                                    respStr = new Scanner(System.in).nextLine();
-                                    byte[] writeBytes = respStr.getBytes();
-                                    ByteBuffer writeBuffer = ByteBuffer.allocate(writeBytes.length);
-                                    writeBuffer.put(writeBytes);
-                                    writeBuffer.flip();
-                                    SocketChannel writeChannel = (SocketChannel) selectionKey.channel();
-                                    writeChannel.write(writeBuffer);
-                                    //注册写通道
-//                                    channel.register(selector,SelectionKey.OP_WRITE);
-                                } else {
-                                    selectionKey.channel().close();
-                                    selectionKey.cancel();
-                                }
-
+                                executorService.submit(new Handle(channel));
                             }
 
                             /*if (selectionKey.isWritable()) {
@@ -200,4 +132,90 @@ public class Server02 {
 
 }
 
+/**
+ * 工作者线程模式
+ */
+class Handle implements Runnable {
 
+
+    private SocketChannel socketChannel;
+
+    public Handle(SocketChannel socektChannel) {
+        this.socketChannel = socektChannel;
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            ByteBuffer byteBuffer = ByteBuffer.allocate(1024);   //这里如果分配的大小不足以写入所有客户端的数据时，会等待一次次写入
+            //将通道中的数据读取到缓存中，通道中的数据就是客户端发送给服务器的数据
+
+            /**
+             * 流程分析
+             *  channel.read(byteBuffer);
+             *      -> SocketChannelImpl
+             *          var3 = IOUtil.read(this.fd, var1, -1L, nd);
+             *              -> IOUtil
+             *                  int var6 = readIntoNativeBuffer(var0, var5, var2, var4);
+             *                      readIntoNativeBuffer
+             *                          var9 = var4.read(var0, ((DirectBuffer)var1).address() + (long)var5, var7);   -> var4 ----->SocketDispatcher
+             *                              -> SocketDispatcher
+             *                                  read0(var1, var2, var4)
+             *                                      调用native方法
+             *                                      static native int read0(FileDescriptor var0, long var1, int var3) throws IOException;
+             *
+             *                                      翻译openjdk这里的源代码
+             *                                          Java_sun_nio_ch_SocketDispatcher_read0(JNIEnv *env, jclass clazz, jobject fdo,
+             *                                       jlong address, jint len)
+             *                                          见下，使用Windows的socket套接字编程，首先是将数据读取到定义的buffers中
+             *                                          然后，调用函数return convertReturnVal(env, (jint)read, JNI_TRUE); 将数据返回、
+             *
+             *
+             *
+             *
+             *   经过测试可以发现的状况是：
+             *          如果发送数据大小超过了读取时定义的ByteBuffer的大小时，会去读取多次，将数据全部读取到。
+             */
+            /* read into the buffers */
+            /*i = WSARecv((SOCKET)fd, *//* Socket *//*
+                                        &buf,           *//* pointers to the buffers *//*
+                                        (DWORD)1,       *//* number of buffers to process *//*
+                                        &read,          *//* receives number of bytes read *//*
+                                        &flags,         *//* no flags *//*
+                                        0,              *//* no overlapped sockets *//*
+                                        0);            */ /* no completion routine */
+
+            int readLength = socketChannel.read(byteBuffer);
+            if (readLength > 0) {
+                //flip NIO中最复杂的操作就是Buffer的控制
+                /**
+                 * Buffer中有一个游标，游标信息在操作后不会归零。如果直接访问Buffer的话，数据有不一致的可能。
+                 * flip是重置游标的方法。在NIO编程中，flip方法是常用方法
+                 */
+                byteBuffer.flip();
+                //字节数组，保存具体数据  byteBuffer.reamining方法是获取Buffer中有效数据长度
+                byte[] bytes = new byte[byteBuffer.remaining()];
+                //将buffer中的数据保存到bytes字节数组中
+                byteBuffer.get(bytes);
+                String requestStr = new String(bytes, "utf-8");
+                System.out.println("请求的数据来源：" + socketChannel.getRemoteAddress() + ",请求的数据为：" + requestStr);
+                String respStr = "哈哈";
+                if ("1".equals(requestStr)) {
+                    respStr = "hello world";
+                } else if ("2".equals(requestStr)) {
+                    respStr = "once complie";
+                }
+                System.out.println("服务器开始响应");
+                byte[] writeBytes = respStr.getBytes();
+                ByteBuffer writeBuffer = ByteBuffer.allocate(writeBytes.length);
+                writeBuffer.put(writeBytes);
+                writeBuffer.flip();
+                socketChannel.write(writeBuffer);
+            }
+        } catch (Exception e) {
+
+        }
+
+    }
+}
