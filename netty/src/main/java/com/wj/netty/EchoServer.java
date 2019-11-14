94524a1818836f0df7cf0af6f0017d9a1de216b4
jie8023yu <2425393776@qq.com>
2019-11-15 00:20:02 +0800

    netty底层源码分析

--- netty/src/main/java/com/wj/netty/EchoServer.java
+++ netty/src/main/java/com/wj/netty/EchoServer.java
@@ -26,6 +26,49 @@ public class EchoServer {
         //线程组
         final EchoServerHandler handler = new EchoServerHandler();
         EventLoopGroup group = new NioEventLoopGroup();
+        /**
+         * 此处代码核心意思讲解
+         *      new NioEventLoopGroup()------->
+         *          this(0);   --------》
+         *              默认传入的线程数量为0，也就可以指定线程数量
+         *               this(nThreads, (Executor) null);------》
+         *                  this(nThreads, executor, SelectorProvider.provider());-----》
+         *                      this(nThreads, executor, selectorProvider, DefaultSelectStrategyFactory.INSTANCE);-----》
+         *                          super(nThreads, executor, selectorProvider, selectStrategyFactory, RejectedExecutionHandlers.reject());----》
+         *                              super是MultithreadEventExecutorGroup这个抽象类，实现了EventLoopGroup接口
+         *                              MultithreadEventLoopGroup-------》
+         *                                  MultithreadEventLoopGroup(int nThreads, Executor executor, Object... args)-----》
+         *                                      super(nThreads == 0 ? DEFAULT_EVENT_LOOP_THREADS : nThreads, executor, args);----》
+         *                                          super是MultithreadEventExecutorGroup这个抽象类，此处传入的默认为0，会创建的线程数为DEFAULT_EVENT_LOOP_THREADS个，这个数据
+         *                                          有静态快加载：如下：
+         *                                              static {
+                                                     *     DEFAULT_EVENT_LOOP_THREADS = Math.max(1, SystemPropertyUtil.getInt(
+                                                     *             "io.netty.eventLoopThreads", NettyRuntime.availableProcessors() * 2));
+                                                     *
+                                                     *     if (logger.isDebugEnabled()) {
+                                                     *         logger.debug("-Dio.netty.eventLoopThreads: {}", DEFAULT_EVENT_LOOP_THREADS);
+                                                     *        }
+                                                     * }
+         *                                          MultithreadEventExecutorGroup------》
+         *                                              MultithreadEventExecutorGroup(int nThreads, Executor executor, Object... args)------》
+         *                                                   this(nThreads, executor, DefaultEventExecutorChooserFactory.INSTANCE, args);-----》
+         *                                                      判断executor是否为空，如果为空，创建一个executor
+         *                                                      if (executor == null) {
+                                                     *             executor = new ThreadPerTaskExecutor(newDefaultThreadFactory());
+                                                     *         }
+         *                                                      创建EventExecutor对象，创建多少个，传入的线程数量，默认传入为0，创建的线程个数为当前机器处理器的个数*2
+         *                                                      先创建EventExecutor数组对象，申请空间
+         *                                                      private final EventExecutor[] children;  定义为类变量
+         *                                                      children = new EventExecutor[nThreads];
+         *                                                      开始for循环创建出nThreads个EventExecutor对象
+         *                                                      children[i] = newChild(executor, args);------》
+         *                                                          NioEventLoopGroup------》
+         *                                                              EventLoop newChild(Executor executor, Object... args)-----》
+         *                                                                  return new NioEventLoop(this, executor, (SelectorProvider) args[0],
+         *                                                                          ((SelectStrategyFactory) args[1]).newSelectStrategy(), (RejectedExecutionHandler) args[2]);
+         *
+         *
+         */
         try {
             //服务器端必备ServerBootStrap
             ServerBootstrap serverBootstrap = new ServerBootstrap();
